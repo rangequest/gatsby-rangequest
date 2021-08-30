834a8c6e0add0194c4707127cc1666cdcdb5dd12
Range Quest <range.quest@gmail.com>
2021-08-30 12:33:16 +0530

    added some plugins to enhance functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Welcome to Gatsby.</p>
+      <StaticImage
+        alt="Gatsby development on VSCode"
+        src="../images/gatsby-vscode-range-quest.png"
+      />
     </Layout>
   )
 }
